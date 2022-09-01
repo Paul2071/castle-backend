6fffb712792fdbfb35c5be2ae4b2632cd53d94bf
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-09-01 11:56:54 +0100

    still deploying to heroku :D

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let db;
 
 connectToDb((err) => {
   if (!err) {
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log("listening on port 3000");
     });
     db = getDb();
